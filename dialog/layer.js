7e7b709bc3d750293414ebb4271fd29e3376b917
jinan <nan.ji@dajie-inc.com>
2013-10-11 16:20:31 +0800

    add dialog
    add dialog

---
+++ dialog/layer.js
@@ -0,0 +1,156 @@
+/**
+ * 最基本的对象
+ * 包括弹层最基本的定位，显示，关闭，填充内容等方法
+ *
+ * 2013 10 09
+ * by jn
+ */
+(function (exports,$) {
+	var layerId = 0,
+		layers = [];
+
+	var defaultSetting = {
+		'cache' : false, // 关闭后是否保留弹层
+		// 位置信息
+		'style' : {
+			'left' : 'auto',
+			'top' : 'auto',
+			'right' : 'auto',
+			'bottom' : 'auto',
+			'width' : 'auto',
+			'height' : 'auto'
+		},
+		// 各种回调和方法
+		'beforeOpen' : null, // open之前的回调
+		'open' : null, // open之后的回调
+		'close' : null, // 关闭之后的回调
+		'wrap' : null, // 外框
+		'content' : null, // 内容，也可以通过setContent来动态插入
+		// 'setWrap' : null, // 设置外框，如果不设置此方法的话，content是什么就会显示什么
+		// 'setContent' : null, // 动态插入内容的方法
+		'assemble' : null // 组装wrap和content
+	};
+
+	function layer(setting) {
+		layerId += 1;
+		this.setting = $.extend({},defaultSetting,setting);
+		this.id = layerId;
+	};
+
+	layer.prototype = {
+		'constructor' : layer,
+		'open' : false, // 是否处于open状态
+		'dom' : null, // dom对象，带有外框
+		'content' : null, // 内容部分
+		// 渲染对象
+		'render' : function () {
+			var layer = this,
+				wrap,content,dom;
+
+			// 渲染外框
+			if ($.isFunction(layer.setting.wrap)) {
+				wrap = layer.setting.wrap();
+			} else if (typeof(layer.setting.wrap) == 'string') {
+				wrap = layer.setting.wrap;
+			} else {
+				wrap = 'body';
+			};
+			layer.wrap = $(wrap);
+
+			// 渲染content
+			if ($.isFunction(layer.setting.content)) {
+				content = layer.setting.content();
+			} else if (typeof(layer.setting.content) == 'string') {
+				content = layer.setting.content;
+			};
+			layer.content = $(content).length ? $(content) : content;
+
+			// 拼接wrap和content
+			if ($.isFunction(layer.setting.assemble)) {
+				layer.setting.assemble(layer.wrap,layer.content);
+				dom = layer.wrap;
+			} else {
+				dom = layer.wrap.append(layer.content);
+			};
+			layer.dom = $(dom).hide();
+		},
+		// 定位
+		'setStyle' : function (options,callback) {
+			var layer = this,
+				option = $.extend({},defaultSetting.style,options);
+			
+			// 定位
+			layer.dom.css(option);
+			// 回调
+			if ($.isFunction(callback)) {
+				callback.call(layer,layer.dom);
+			};
+		},
+		'open' : function (callback) {
+			var layer = this;
+
+			layer.render();
+			layer.setStyle(layer.setting.style);
+
+			layer.dom.appendTo('body');
+
+			// 回调
+			if ($.isFunction(layer.setting.beforeOpen)) {
+				layer.setting.beforeOpen.call(layer,layer.dom);
+			};
+			//显示弹层
+			layer.dom.show();
+
+			// 回调
+			if ($.isFunction(callback)) {
+				callback.call(layer,layer.dom);
+			} else if ($.isFunction(layer.setting.open)) {
+				layer.setting.open.call(layer,layer.dom);
+			};
+		},
+		'close' : function (callback) {
+			var layer = this;
+			if (layer.setting.cache) {
+				layer.dom.fadeOut(function () {
+					if ($.isFunction(callback)) {
+						callback.call(layer,layer.dom);
+					} else if ($.isFunction(layer.setting.close)) {
+						layer.setting.close.call(layer,layer.dom);
+					};
+				});
+			} else {
+				layer.dom.fadeOut(function () {
+					layer.destroy();
+					if ($.isFunction(callback)) {
+						callback.call(layer,layer.dom);
+					} else if ($.isFunction(layer.setting.close)) {
+						layer.setting.close.call(layer,layer.dom);
+					};
+				});
+			};
+		},
+		// 插入内容
+		'setContent' : function (content) {
+			var layer = this;
+
+			layer.content = content;
+
+			// 拼接wrap和content
+			if ($.isFunction(layer.setting.assemble)) {
+				dom = layer.setting.assemble(layer.wrap,layer.content);
+			} else {
+				dom = layer.wrap.html(layer.content);
+			};
+		},
+		'destroy' : function () {
+			var layer = this;
+			layer.dom.remove();
+			layer = null;
+		}
+	};
+
+	// helper
+
+
+	exports.layer = layer;
+})(window,jQuery);
---
+++ dialog/tips.js
@@ -0,0 +1,130 @@
+/**
+ * 生成tips
+ * 依赖于layer.js
+ *
+ * 2013 10 06
+ * by jn
+ */
+(function (exports,$) {
+	// 内部变量
+	var tipsId = 0;
+
+	// tip模版
+	var wrapTpl = '<div data-tipid="commenTip-<%= tipsId %>" class="Y_commenTips <%= tipClass %> <%= arrow %>" id="<%= id %>">' + 
+					'<div class="bd">'+
+					'</div>' + 
+					'<% if (pointer) { %>' + 
+						'<em class="pointer"></em>' + 
+					'<% } %>' + 
+					'<% if (closeBtn) { %>' + 
+						'<a class="close" href="#"></a>' + 
+					'<% } %>' + 
+				  '</div>';
+
+	// 默认设置
+	var defaultSetting = {
+		'pointer' : true, // 是否显示箭头
+		'closeBtn' : true, // 是否显示关闭按钮
+		'content' : '', // 内容
+		'left' : 'auto',
+		'right' : 'auto',
+		'top' : 'auto',
+		'bottom' : 'auto',
+		'width' : '100px',
+		'height' : 'auto',
+		'horizontal' : '0', // 水平偏移位置
+		'vertical' : '0', // 垂直偏移位置
+		'arrow' : 'bottom-tip',
+		'tipClass' : '',
+		'id' : '',
+		'open' : null,
+		'close' : null,
+		'tipsId' : tipsId
+	};
+
+	// 原始tips对象
+	function Tips(setting) {
+		tipsId += 1;
+		this.setting = $.extend({},defaultSetting,setting);
+		this.id = tipsId;
+		this.setting.tipsId = this.id;
+
+		var wrapRender = template.compile(wrapTpl),
+			wrap = wrapRender(this.setting);
+
+		this.layer = new layer({
+			'cache' : false,
+			'style' : {
+				'width' : this.setting.width,
+				'heigith' : this.setting.height,
+				'left' : this.setting.left,
+				'right' : this.setting.right,
+				'top' : this.setting.top,
+				'bottom' : this.setting.bottom,
+				'position' : 'absolute'
+			},
+			'wrap' : wrap,
+			'content' : this.setting.content,
+			'beforeOpen' : this.setting.beforeOpen,
+			'open' : this.setting.open,
+			'close' : this.setting.close,
+			'assemble' : function (wrap,content) {
+				wrap.find('.bd').html(content);
+			}
+		});
+	};
+	Tips.prototype = {
+		'constructor' : Tips,
+		'open' : function (callback) {
+			this.layer.open(callback)
+		},
+		'close' : function (callback) {
+			this.layer.close(callback)
+		},
+		'destroy' : function (callback) {
+			this.layer.destroy();
+			this = null;
+		}
+	};
+
+	// 自动获取位置的tips方法
+	function triggerTips(setting) {
+		this.setting = setting;
+		this.init();
+	};
+	triggerTips.prototype = {
+		'constructor' : triggerTips,
+		'trigger' : null,
+		'init' : function () {
+			var triggerTip = this,
+				arrow = triggerTip.setting.arrow || 'bottom',
+				trigger = $(triggerTip.setting.trigger),
+				left = trigger.offset().left,
+				top = trigger.offset().top;
+
+			triggerTip.trigger = trigger;
+			triggerTip.tip = new Tips({
+				'left' : left,
+				'top' : top,
+				'open' : function (layerDom) {
+					console.log(layerDom.height())
+					var layer = this,
+						fixedTop = top - layerDom.height() - 7,
+						fixedLeft = left + trigger.width() / 2;
+
+					layer.setStyle({
+						'top' : fixedTop,
+						'left' : fixedLeft
+					});
+				}
+			});
+
+			triggerTip.tip.open();
+		}
+	};
+
+
+
+	exports.Tips = Tips;
+	exports.triggerTips = triggerTips;
+})(window,jQuery);
